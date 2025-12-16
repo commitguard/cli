a2f8b27dcd2a02c12781cb359f029ebda2652e9a
Moshe Tanzer <tanzermt@gmail.com>
2025-12-16 20:38:46 +0200

    feat: new optimized git commit ignore parser and diff creator

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,39 +1,25 @@
-import { execSync } from 'node:child_process'
-import micromatch from 'micromatch'
+import { execFileSync } from 'node:child_process'
 
 export function getStagedDiff(ignorePatterns: string[]): string {
   try {
-    // TODO: let user set speed vs accuracy tradeoff which we should use unifed=7 for speed and no function-context
-    const fullDiff = execSync(
-      'git diff --cached --no-color --minimal --ignore-all-space --function-context --diff-algorithm=histogram --diff-filter=AMC',
-      {
-        encoding: 'utf8',
-        maxBuffer: 10 * 1024 * 1024,
-        stdio: ['pipe', 'pipe', 'ignore'],
-      },
-    )
-
-    if (!ignorePatterns.length)
-      return fullDiff
-
-    return filterDiffByPatterns(fullDiff, ignorePatterns)
+    const args = [
+      'diff',
+      '--cached',
+      '--no-color',
+      '--function-context',
+      '--diff-algorithm=histogram',
+      '--diff-filter=AMC',
+      '--',
+      '.',
+      ...ignorePatterns.map(p => `:(exclude)${p}`),
+    ]
+    return execFileSync('git', args, {
+      encoding: 'utf8',
+      maxBuffer: 10 * 1024 * 1024,
+      stdio: ['pipe', 'pipe', 'ignore'],
+    })
   }
   catch {
     return ''
   }
-}
-
-function filterDiffByPatterns(diff: string, patterns: string[]): string {
-  const sections = diff.split(/^diff --git /m).filter(Boolean)
-
-  const filtered = sections.filter((section) => {
-    const match = section.match(/^a\/(.*?) b\//)
-    if (!match)
-      return true
-
-    const filepath = match[1]
-    return !micromatch.isMatch(filepath, patterns, { dot: true, matchBase: true })
-  })
-
-  return filtered.length ? `diff --git ${filtered.join('diff --git ')}` : ''
 }
