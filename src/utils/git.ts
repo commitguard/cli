08ab0eb22168cdc6afc634189aeb535a788c65ad
Moshe Tanzer <tanzermt@gmail.com>
2025-12-19 09:50:57 +0200

    feat: add get last diff function for bypass

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -23,3 +23,28 @@ export function getStagedDiff(ignorePatterns: string[]): string {
     return ''
   }
 }
+
+export function getLastDiff(ignorePatterns: string[]): string {
+  try {
+    const args = [
+      'diff',
+      'HEAD~1',
+      'HEAD',
+      '--no-color',
+      '--function-context',
+      '--diff-algorithm=histogram',
+      '--diff-filter=AMC',
+      '--',
+      '.',
+      ...ignorePatterns.map(p => `:(exclude)${p}`),
+    ]
+    return execFileSync('git', args, {
+      encoding: 'utf8',
+      maxBuffer: 10 * 1024 * 1024,
+      stdio: ['pipe', 'pipe', 'ignore'],
+    })
+  }
+  catch {
+    return ''
+  }
+}
